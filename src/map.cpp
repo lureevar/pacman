0530444b06c767c69135d703abd147f7ff225a54
Luiz Felipe <luizfelipecastrovb@gmail.com>
2022-07-09 17:14:35 -0300

    Use makefile && Refactor (#6)

--- src/map.cpp
+++ src/map.cpp
@@ -1,55 +1,102 @@
 #include <fstream>
-#include <iostream>
 
-#include "map.h"
+#include "../include/map.hpp"
 
-Map::Map(const char *file)
-{
-    this->scene = new int*[HEIGHT];
-    for (int i = 0; i < WIDTH; i++)
-    {
-        this->scene[i] = new int[WIDTH];
-    }
+#define DEFAULT_PATH_MAP "data/map/classic.map"
+#define DEFAULT_BITMAPS_WIDTH 32
+#define DEFAULT_BITMAPS_HEIGHT 32
 
-    std::ifstream in(file);
+Map::Map(ALLEGRO_BITMAP *pill, ALLEGRO_BITMAP *wall, ALLEGRO_BITMAP *super_pill, ALLEGRO_BITMAP *nothing) {
+	this->count_pill = this->count_super_pill = 0;
 
-    for (int i = 0; i < WIDTH; i++)
-    {
-        for (int j = 0; j < HEIGHT; j++)
-        {
-            in >> this->scene[i][j];
-        }
-    }
+	this->path_map = DEFAULT_PATH_MAP;
+
+	this->pill = pill;
+	this->wall = wall;
+	this->super_pill = super_pill;
+	this->nothing = nothing;
+
+	this->set_scene();
 }
 
-Map::~Map(void)
-{
-    for (int i = 0; i < WIDTH; i++)
-    {
-        delete [] this->scene[i];
-    }
+Map::Map(const Map &map) {
+	this->operator=(map);
 
-    delete [] this->scene;
+	this->set_scene();
 }
 
-void Map::draw(ALLEGRO_BITMAP *wall, ALLEGRO_BITMAP *pill, ALLEGRO_BITMAP *super_pill)
-{
-    for (int i = 0; i < WIDTH; i++)
-    {
-        for (int j = 0; j < HEIGHT; j++)
-        {
-            switch (this->scene[i][j])
-            {
-            case WALL:
-                al_draw_bitmap(wall, j * 32, i * 32, 0);
-                break;
-            case PILL:
-                al_draw_bitmap(pill, j * 32, i * 32, 0);
-                break;
-            case SUPER_PILL:
-                al_draw_bitmap(super_pill, j * 32, i * 32, 0);
-                break;
-            }
-        }
-    }
+int Map::get_count_pill(void) const {
+	return this->count_pill;
+}
+
+int Map::get_count_super_pill(void) const {
+	return this->count_pill;
+}
+
+maps_objects_e Map::get_object_scene(const int row, const int column) const {
+	return this->scene[row][column];
+}
+
+void Map::draw_scene(void) const {
+	for (int i = 0; i < DEFAULT_ROWS; i++) {
+		for (int j = 0; j < DEFAULT_COLUMNS; j++) {
+			switch (this->scene[i][j]) {
+			case WALL:
+				al_draw_bitmap(this->wall, j * DEFAULT_BITMAPS_HEIGHT, i * DEFAULT_BITMAPS_WIDTH, 0);
+				break;
+			case PILL:
+				al_draw_bitmap(this->pill, j * DEFAULT_BITMAPS_HEIGHT, i * DEFAULT_BITMAPS_WIDTH, 0);
+				break;
+			case SUPER_PILL:
+				al_draw_bitmap(this->super_pill, j * DEFAULT_BITMAPS_HEIGHT, i * DEFAULT_BITMAPS_WIDTH, 0);
+				break;
+			default:
+				al_draw_bitmap(this->nothing, j * DEFAULT_BITMAPS_HEIGHT, i * DEFAULT_BITMAPS_WIDTH, 0);
+			}
+		}
+	}
+}
+
+Map &Map::operator=(const Map &map) {
+	if (this != &map) {
+		this->count_pill = map.count_pill;
+		this->count_super_pill = map.count_super_pill;
+
+		this->pill = map.pill;
+		this->super_pill = map.super_pill;
+		this->wall = map.wall;
+		this->nothing = map.nothing;
+
+		for (int i = 0; i < DEFAULT_ROWS; i++)
+			for (int j = 0; j < DEFAULT_COLUMNS; j++)
+				this->scene[i][j] = map.scene[i][j];
+	}
+
+	return *this;
+}
+
+void Map::set_scene(void) {
+	std::ifstream in(this->path_map);
+
+	for (int i = 0; i < DEFAULT_ROWS; i++) {
+		for (int j = 0; j < DEFAULT_COLUMNS; j++) {
+			int object_id;
+
+			in >> object_id;
+
+			this->scene[i][j] = static_cast<maps_objects_e>(object_id);
+
+			switch (this->scene[i][j]) {
+			case SUPER_PILL:
+				this->count_super_pill++;
+				break;
+			case PILL:
+				this->count_pill++;
+				break;
+			case NOTHING:
+			case WALL:
+				break;
+			}
+		}
+	}
 }
